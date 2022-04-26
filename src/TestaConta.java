695d2d3d18e68b12449079a06dae8a114353d8c3
Karen Frazão <karen.pfr00@gmail.com>
2022-04-26 20:29:38 -0300

    Realizaçao da Media de compras

--- src/TestaConta.java
+++ src/TestaConta.java
@@ -55,6 +55,7 @@ public class TestaConta {
         System.out.println(menosCompras);
         System.out.println("--------------------");
 
+        System.out.println(clientes.stream().mapToDouble(Cliente::getCompras).average().getAsDouble());
 
 
     }
